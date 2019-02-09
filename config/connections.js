c6718adbae1c02803d7314e8e30ea37fd0efca39
Blue Buffalo <anettagoldsher@gmail.com>
2019-02-09 16:35:54 -0700

    jaws update

--- config/connections.js
+++ config/connections.js
@@ -1,6 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var connection;
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    })
+}
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,13 +20,13 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
