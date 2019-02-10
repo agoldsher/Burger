c34db1c8cace302a9eba13beda634a4a78f4f247
Blue Buffalo <anettagoldsher@gmail.com>
2019-02-09 21:14:04 -0700

    now its broken

--- config/connections.js
+++ config/connections.js
@@ -1,32 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
-if (process.env.JAWSBD_URL) {
-    connection = mysql.createConnection(process.env.JAWSBD_URL);
+require('dotenv').config();
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "burgers_db"
+        host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "gv9zxyslvgcyvi23",
+        password: "r4wwu3r4sezmyfbj",
+        database: "jx12svlufm52o8km"
     })
 }
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+
 
 // Make connection.
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return err;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
